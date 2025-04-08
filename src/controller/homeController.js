d1de3ac5981f3c434fb3ff222f47cd29680e17a8
Luan2912 <basonam35@gmail.com>
2025-04-09 06:02:28 +0700

    video 4 create new user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,12 @@
+import mysql from 'mysql2';
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'jwt',
+});
+
 const handleHelloWorld = (req, res) =>{
     return res.render("home.ejs");
 }
@@ -6,7 +15,28 @@ const handleUserPage = (req, res) => {
     return res.render("user.ejs");
 }
 
+const handleCreateNewUser = (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let username = req.body.username;
+
+
+    connection.query(
+        'INSERT INTO users(email,password,username) VALUES(?,?,?)', [email,password,username],
+        function(err, results, fields) {
+           if(err){
+            console.log(err);
+           }
+           console.log(results);
+        }
+      );
+    
+
+    return res.send("handleCreateNewUser");
+};
+
 module.exports = {
     handleHelloWorld,
-    handleUserPage
+    handleUserPage,
+    handleCreateNewUser
 }
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import configViewEngine from './configs/viewEngine';
 import initWebRouters from './routes/web';
 require("dotenv").config();
+import bodyParser from 'body-parser';
+
 
 const app = express();
 //Khai báo cổng được định nghĩa sẵn trong '.env.(file chứa các biến môi trường)', nếu chưa có thì mặc định PORT = 8080
@@ -10,9 +12,14 @@ const PORT = process.env.PORT || 8080;
 //config view engine
 configViewEngine(app);
 
+
+//config bodyParser
+app.use(bodyParser.json()); // cho application/json
+app.use(bodyParser.urlencoded({ extended: true })); // cho form data
+
+
 //init web routers
 initWebRouters(app);
-
 
 app.listen(PORT, () => {
     console.log("JWT Backend is running on the PORT = " + PORT);
